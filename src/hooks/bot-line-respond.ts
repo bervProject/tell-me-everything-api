7fb8c7ce8a2f1f0444013f6705215c90c9cac9c7
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-02-01 13:08:44 +0700

    Add Auto Response join

--- src/hooks/bot-line-respond.ts
+++ src/hooks/bot-line-respond.ts
@@ -3,6 +3,7 @@
 import { Hook, HookContext } from '@feathersjs/feathers';
 import logger from '../logger';
 import client from '../line-client';
+import { JoinEvent, MemberJoinEvent } from '@line/bot-sdk';
 
 export default (options = {}): Hook => {
   return async (context: HookContext) => {
@@ -11,28 +12,39 @@ export default (options = {}): Hook => {
     logger.info(JSON.stringify(headers));
     logger.info(JSON.stringify(data));
     const event = data.events[0];
+    const eventType = event.type;
     const sourceType = event.source.type;
     logger.info(JSON.stringify(event));
-    if (event.type === "message") {
+    if (eventType === "message") {
       const message = event.message;
       const messageType = message.type;
       const messageText = message.text.toLowerCase();
       logger.info(JSON.stringify(message));
       if (messageType === "text" && messageText === "bye") {
         if (sourceType === "room") {
-          client.leaveRoom(event.source.roomId);
+          await client.leaveRoom(event.source.roomId);
         } else if (sourceType === "group") {
-          client.leaveGroup(event.source.groupId);
+          await client.leaveGroup(event.source.groupId);
         } else {
-          client.replyMessage(event.replyToken, {
+          await client.replyMessage(event.replyToken, {
             type: "text",
             text: "I cannot leave a 1-on-1 chat!",
           });
         }
       } else if (messageType == "text" && messageText == "hi") {
-        client.replyMessage(event.replyToken, {
+        await client.replyMessage(event.replyToken, {
           type: "text",
           text: "Hello World!"
+        });
+      }
+    } else if (eventType === "memberJoined") {
+      const joinEvent: MemberJoinEvent = event;
+      const joinedMembers = joinEvent.joined.members;
+      for (const member of joinedMembers) {
+        const profile = await client.getProfile(member.userId);
+        await client.replyMessage(event.replyToken, {
+          type: "text",
+          text: `Hi, ${profile.displayName}! Welcome to the group!`
         });
       }
     }
