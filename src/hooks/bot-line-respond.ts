07a636a75fcb79e2d8e5e1eb8027a03d8ff1e000
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-02-01 08:42:00 +0700

    Add log

--- src/hooks/bot-line-respond.ts
+++ src/hooks/bot-line-respond.ts
@@ -1,12 +1,18 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers';
+import logger from '../logger';
+//import Client from '../line-client';
 
 export default (options = {}): Hook => {
   return async (context: HookContext) => {
     const { headers } = context.params;
     const { data } = context;
+    logger.info(JSON.stringify(headers));
+    logger.info(JSON.stringify(data));
+    //Client.pushMessage()
     context.statusCode = 200;
+    context.result = data;
     return context;
   };
 }
