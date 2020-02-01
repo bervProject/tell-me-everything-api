86a5378836220a16b03889aa5f8fad8340b7fc71
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-02-01 23:22:38 +0700

    Improve with type checking

--- src/hooks/bot-line-respond.ts
+++ src/hooks/bot-line-respond.ts
@@ -3,42 +3,53 @@
 import { Hook, HookContext } from '@feathersjs/feathers';
 import logger from '../logger';
 import client from '../line-client';
-import { MemberJoinEvent } from '@line/bot-sdk';
+import {
+  MemberJoinEvent,
+  MessageEvent,
+  TextEventMessage,
+  WebhookEvent,
+  WebhookRequestBody,
+  Room,
+  Group
+} from '@line/bot-sdk';
 
-async function handleEvent(event: any) {
+async function handleEvent(event: WebhookEvent) {
   logger.info(JSON.stringify(event));
   const eventType = event.type;
   const sourceType = event.source.type;
   logger.info(eventType);
   if (eventType === "message") {
-    const message = event.message;
-    const messageType = message.type;
-    const messageText = message.text.toLowerCase();
+    const messageEvent = event as MessageEvent;
+    const message = messageEvent.message;
     logger.info(JSON.stringify(message));
-    if (messageType === "text" && messageText === "bye") {
-      if (sourceType === "room") {
-        await client.leaveRoom(event.source.roomId);
-      } else if (sourceType === "group") {
-        await client.leaveGroup(event.source.groupId);
-      } else {
-        await client.replyMessage(event.replyToken, {
+    const messageType = message.type;
+    if (messageType === "text") {
+      const messageText = (message as TextEventMessage).text.toLowerCase();
+      if (messageText === "bye") {
+        if (sourceType === "room") {
+          await client.leaveRoom((event.source as Room).roomId);
+        } else if (sourceType === "group") {
+          await client.leaveGroup((event.source as Group).groupId);
+        } else {
+          await client.replyMessage(messageEvent.replyToken, {
+            type: "text",
+            text: "I cannot leave a 1-on-1 chat!",
+          });
+        }
+      } else if (messageText == "hi") {
+        await client.replyMessage(messageEvent.replyToken, {
           type: "text",
-          text: "I cannot leave a 1-on-1 chat!",
+          text: "Hello World!"
         });
       }
-    } else if (messageType == "text" && messageText == "hi") {
-      await client.replyMessage(event.replyToken, {
-        type: "text",
-        text: "Hello World!"
-      });
     }
   } else if (eventType === "memberJoined") {
-    const joinEvent: MemberJoinEvent = event;
+    const joinEvent = event as MemberJoinEvent;
     const joinedMembers = joinEvent.joined.members;
     for (const member of joinedMembers) {
       logger.info(`Welcome to ${member.userId}`);
       const profile = await client.getProfile(member.userId);
-      await client.replyMessage(event.replyToken, {
+      await client.replyMessage(joinEvent.replyToken, {
         type: "text",
         text: `Hi, ${profile.displayName}! Welcome to the group!`
       });
@@ -50,7 +61,8 @@ export default (options = {}): Hook => {
   return async (context: HookContext) => {
     const { data } = context;
     logger.info(JSON.stringify(data));
-    for (event of data.events) {
+    const events = (data as WebhookRequestBody).events;
+    for (const event of events) {
       handleEvent(event);
     }
     context.statusCode = 200;
