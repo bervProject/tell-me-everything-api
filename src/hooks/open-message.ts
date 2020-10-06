1f4306bd27c75a8660ed112584ea6ce1a6e95749
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-10-07 00:52:35 +0700

    update type

--- src/hooks/open-message.ts
+++ src/hooks/open-message.ts
@@ -2,6 +2,7 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers';
 import AES from 'crypto-js/aes';
+import Utf8 from 'crypto-js/enc-utf8';
 
 export default (options = {}): Hook => {
   return async (context: HookContext) => {
@@ -11,10 +12,9 @@ export default (options = {}): Hook => {
       return context;
     }
     const { result } = context;
-    const output = AES.decrypt(result.text, messagePassword).toString();
-    const outputString = decodeURIComponent(output.replace(/\s+/g, '').replace(/[0-9a-f]{2}/g, '%$&'));
+    const output = AES.decrypt(result.text, messagePassword).toString(Utf8);
     context.result = Object.assign(result, {
-      text: outputString
+      text: output
     });
     return context;
   };
