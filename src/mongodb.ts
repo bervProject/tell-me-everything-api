a550c881fed411935dd4426dd603d8d9f0e22fb8
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-05-25 15:34:39 +0700

    Add error handling

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -1,12 +1,15 @@
 import { MongoClient } from "mongodb";
 import { Application } from "./declarations";
+import logger from "./logger";
 
 export default function (app: Application): void {
   const connection = app.get("mongodb");
   const database = connection.substr(connection.lastIndexOf("/") + 1);
   const mongoClient = MongoClient.connect(connection, {
     useNewUrlParser: true,
-  }).then((client) => client.db(database));
+  })
+    .then((client) => client.db(database))
+    .catch((error) => logger.error(JSON.stringify(error)));
 
   app.set("mongoClient", mongoClient);
 }
