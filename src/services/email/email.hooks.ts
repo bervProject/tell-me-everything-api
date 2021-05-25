931d54dd81297c1fb5a5aaf50e77d67f721c3c19
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-05-25 18:17:38 +0700

    Add jwt check

--- src/services/email/email.hooks.ts
+++ src/services/email/email.hooks.ts
@@ -1,10 +1,14 @@
 import { disallow } from "feathers-hooks-common";
+import * as feathersAuthentication from "@feathersjs/authentication";
 import checkPermissions from "feathers-permissions";
 import { iff } from "feathers-hooks-common";
+
+const { authenticate } = feathersAuthentication.hooks;
 
 export default {
   before: {
     all: [
+      authenticate("jwt"),
       checkPermissions({
         roles: ["admin"],
         error: false,
