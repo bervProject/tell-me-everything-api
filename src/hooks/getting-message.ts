18bc7296dd2e3b77fddb9b971f33b463b30d502a
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-07-14 19:54:25 +0700

    Change hash

--- src/hooks/getting-message.ts
+++ src/hooks/getting-message.ts
@@ -1,22 +1,26 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+import { Forbidden, NotFound } from "@feathersjs/errors";
 import { Hook, HookContext } from "@feathersjs/feathers";
-import Base64 from "crypto-js/enc-base64";
-import SHA512 from "crypto-js/sha512";
+import bcryptjs from "bcryptjs";
 
 export default (options = {}): Hook => {
   return async (context: HookContext) => {
-    const { params, app } = context;
+    const { id, params, app } = context;
     const { headers } = params;
-    const mySalt = app.get("salt");
     const messagePassword = headers?.key;
     if (!messagePassword) {
-      return context;
+      throw new Forbidden("You are not authorized to check this messages");
     }
-    const hashPassword = Base64.stringify(SHA512(messagePassword + mySalt));
-    context.params.query = {
-      messagePassword: hashPassword,
-    };
+    const data = await app.service("message")._get(id);
+    if (!data) {
+      throw new NotFound("Data Not Found!");
+    }
+    const originalPassword = data.messagePassword;
+    const same = bcryptjs.compareSync(messagePassword, originalPassword);
+    if (!same) {
+      throw new Forbidden("You are not authorized to check this messages");
+    }
     return context;
   };
 };
--- src/hooks/messages-encrypt.ts
+++ src/hooks/messages-encrypt.ts
@@ -2,17 +2,14 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from "@feathersjs/feathers";
 import AES from "crypto-js/aes";
-import Base64 from "crypto-js/enc-base64";
-import SHA512 from "crypto-js/sha512";
+import bcryptjs from "bcryptjs";
 
 export default (options = {}): Hook => {
   return async (context: HookContext) => {
-    const { app } = context;
-    const mySalt = app.get("salt");
     const data = context.data;
     const text = data.text;
     const password = data.messagePassword;
-    const hashPassword = Base64.stringify(SHA512(password + mySalt));
+    const hashPassword = bcryptjs.hashSync(password);
     const textEncrypted = AES.encrypt(text, password).toString();
     context.data = Object.assign(data, {
       text: textEncrypted,
