213d9002c403465be5aa6c884d3160b005187618
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-05-22 00:12:49 +0700

    Add instance role for send email

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as apprunner from "@aws-cdk/aws-apprunner-alpha";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 
@@ -21,7 +22,11 @@ export class TmeStack extends cdk.Stack {
       "dev/AppRunner/tme",
     );
 
+    // temporary role, reuse lambda
+    const role = iam.Role.fromRoleName(this, "tme-role", "S3RoleLambda");
+
     const appRunner = new apprunner.Service(this, "tme-apprunner", {
+      instanceRole: role,
       source: apprunner.Source.fromEcr({
         repository: repo,
         imageConfiguration: {
