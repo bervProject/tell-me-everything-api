502e5dbea329628948ff4db660b1e6dcb499dd7c
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-07-18 11:09:56 +0700

    Revert "fix: temporary ignore"
    This reverts commit 9ef80e6553e75c9f15d3c3c95dd9772495310db2.

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -3,42 +3,35 @@ import { Application } from "./declarations";
 import logger from "./logger";
 
 export default function (app: Application): void {
-  try {
-    const connectionString = app.get("postgres");
-    const sequelize = new Sequelize(connectionString, {
-      dialect: "postgres",
-      logging: false,
-      define: {
-        freezeTableName: true,
-      },
+  const connectionString = app.get("postgres");
+  const sequelize = new Sequelize(connectionString, {
+    dialect: "postgres",
+    logging: false,
+    define: {
+      freezeTableName: true,
+    },
+  });
+  const oldSetup = app.setup;
+
+  app.set("sequelizeClient", sequelize);
+
+  app.setup = function (...args) {
+    const result = oldSetup.apply(this, args);
+
+    // Set up data relationships
+    const models = sequelize.models;
+    Object.keys(models).forEach((name) => {
+      if ("associate" in models[name]) {
+        (models[name] as any).associate(models);
+      }
     });
-    const oldSetup = app.setup;
 
-    app.set("sequelizeClient", sequelize);
+    // Sync to the database
+    app.set(
+      "sequelizeSync",
+      sequelize.sync().catch((err) => logger.error(JSON.stringify(err))),
+    );
 
-    app.setup = function (...args) {
-      const result = oldSetup.apply(this, args);
-
-      // Set up data relationships
-      const models = sequelize.models;
-      Object.keys(models).forEach((name) => {
-        if ("associate" in models[name]) {
-          (models[name] as any).associate(models);
-        }
-      });
-
-      // Sync to the database
-      app.set(
-        "sequelizeSync",
-        sequelize.sync().catch((err) => logger.error(JSON.stringify(err))),
-      );
-
-      return result;
-    };
-  } catch (error) {
-    logger.error("Error setting up Sequelize:", error);
-    // debug only
-    const connectionString = app.get("postgres");
-    logger.warn("Connection string:", connectionString);
-  }
+    return result;
+  };
 }
